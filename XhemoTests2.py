514a88176a2a8a92d44c3ae4c8eded0f76a0f553
xhemo119 <dzemali.s@hotmail.com>
2024-12-04 10:45:14 +0100

    Create XhemoTests2.py

---
+++ XhemoTests2.py
@@ -0,0 +1,28 @@
+# ich versuche hier bisschen Graph distances.ipynb nachzubauen
+
+import networkx as nx
+
+import construction as cs
+import distances as dis
+
+# %load_ext autoreload
+# %autoreload 2
+
+
+dataset1 = "InVS13" # name of the dtaset 1
+dataset2 = "LH10"   # name of the dataset 2
+gap = 299  # temporal gap
+
+structure_type = "ETM" # ETM or ETN
+k = 3 # number of EN used for the creation of ETN
+
+
+# statistical test parameters
+alpha=0.01
+beta=0.1
+gamma=5
+label=False # labels on nodes or not.
+
+dist_etmm = dis.etmm_distance([dataset1,dataset2],structure_type,gap,k,label,alpha,beta,gamma)
+
+print("Distance between \n\t%s and %s \t\t%0.3f" %(dataset1,dataset2,dist_etmm))
