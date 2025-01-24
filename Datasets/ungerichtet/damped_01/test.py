0a047d08d49a2fb7208d2d693ffc1beccf7fbebd
xhemo119 <dzemali.s@hotmail.com>
2025-01-24 02:52:24 +0100

    test

--- Datasets/ungerichtet/damped_01/test.py
+++ Datasets/ungerichtet/damped_01/test.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-x = np.load("Datasets/ungerichtet/damped_01/damped_01_time_series.npy")
+#x = np.load("Datasets/ungerichtet/damped_01/damped_01_time_series.npy")
 
-print(np.shape(x))
+#print(np.shape(x))
+
+
+
+print(np.linspace(0, 300, 31))
