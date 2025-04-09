cc3888c3a14f2ebeed958783341dd9b9ae187215
xhemo119 <dzemali.s@hotmail.com>
2025-04-09 17:13:43 +0200

    zeitreihen ergebnisse

--- Xhemo_CharlotteDaten_ungerichtet_damped.py
+++ Xhemo_CharlotteDaten_ungerichtet_damped.py
@@ -7,209 +7,211 @@ from ETN import *
 from ETMM import *
 
 
-# Parameters
-k = 4                                   # number of static snapshot used for the constructions of ETN
-gap = 0.5                               # temporal gap
-label = False                           # if true, the loaded dataset is labeled
-file_name = "periodic_01_graph_1"
+for baba in range(11, 12):
+
+    # Parameters
+    k = 4                                   # number of static snapshot used for the constructions of ETN
+    gap = 0.5                               # temporal gap
+    label = False                           # if true, the loaded dataset is labeled
+    file_name = "chaotic_01_graph_"+str(baba)
 
 
-def load_data(path):                                                        # Ein spezielles load_data(), da die Daten von Charlotte so ausgelegt sind, dass {'weight': 1} als zwei array elemente gelten (wegen dem Leerzeichen)
-                                                                            # und nicht mehr nur 3 array elemente berücksichtig werden müssen (wie es im originalen ist) sondern 4
-    data = []
-    with open(path) as f:
-        for line in f:
-            tmp = line.split()[0:4]
-            tmp[2] = tmp[2]+tmp[3]                                          # Das hier wird gemacht, um "{'weight':" und "1}" als 1 Element zu haben
-            tmp[2] = int(1)                                                 # Und das wird gemacht, um aus "{'weight':1}" den integer 1 zu machen (kp ob ich das so machen soll mit der 1, aber falls nicht, dann kann ich hier easy diese Zeile einfach rausnehmen 
-            arr_tmp = [int(tmp[0]),int(tmp[1]),int(tmp[2])]                 # und "{'weight':1}" wird korrekt an der Stelle im array gespeichert)
+    def load_data(path):                                                        # Ein spezielles load_data(), da die Daten von Charlotte so ausgelegt sind, dass {'weight': 1} als zwei array elemente gelten (wegen dem Leerzeichen)
+                                                                                # und nicht mehr nur 3 array elemente berücksichtig werden müssen (wie es im originalen ist) sondern 4
+        data = []
+        with open(path) as f:
+            for line in f:
+                tmp = line.split()[0:4]
+                tmp[2] = tmp[2]+tmp[3]                                          # Das hier wird gemacht, um "{'weight':" und "1}" als 1 Element zu haben
+                tmp[2] = int(1)                                                 # Und das wird gemacht, um aus "{'weight':1}" den integer 1 zu machen (kp ob ich das so machen soll mit der 1, aber falls nicht, dann kann ich hier easy diese Zeile einfach rausnehmen 
+                arr_tmp = [int(tmp[0]),int(tmp[1]),int(tmp[2])]                 # und "{'weight':1}" wird korrekt an der Stelle im array gespeichert)
+            
+                data.append(arr_tmp)
+        data = np.array(data)
+        return(data)
+
+
+
+    # Load the temporal graph as a sequence of static NetworkX graphs
+    data = load_data("Datasets/ungerichtet/chaotic_01/"+file_name)                   # hier nicht vergessen immer die Endungen (zB .txt) zu ändern, wenn oben file_name geändert wird
+
+    #print(data)
+
+    def reorder_array(data):
         
-            data.append(arr_tmp)
-    data = np.array(data)
-    return(data)
+        reordered_data = np.zeros_like(data)                            # Erstellt ein Array der gleichen Form wie data
+        
+        reordered_data[:, 0] = data[:, -1]                              # Verschiebt die letzte Spalte auf die erste
+        reordered_data[:, 1] = data[:, 0]                               # Verschiebt die erste Spalte auf die zweite
+        reordered_data[:, 2] = data[:, 1]                               # Verschiebt die zweite Spalte auf die dritte
+        
+        return reordered_data
+
+
+    # Umordnen des Arrays
+    reordered_data = reorder_array(data)
+    #print(reordered_data)
+
+
+    def time_changed_data(reordered_data):                                              # da Charlottes Daten an der Stelle, wo eig die Zeit stehen muss, {'weight': 1} stehen haben und sie in der E-Mail meinte die Daten sind 
+        new_data = np.zeros_like(reordered_data)                                        # in der richtigen Reihenfolge platziert alle 0.5 Sekunden, habe ich dann diese Funktion programmiert 
+        new_data = new_data.astype(object)                                              # (Change to object type to allow mixed types)
+        new_data[:, 0] = np.arange(0, len(reordered_data[:,0]) * 0.5, 0.5)              # die an der Stelle von 0 hochzählt und in jeder inkrementation (0.5, 1, 1.5, ...) reinschreibt
+        new_data[:, 1] = reordered_data[:, 1]
+        new_data[:, 2] = reordered_data[:, 2]
+        return new_data
+
+
+    new_data = time_changed_data(reordered_data)
+
+    #print(new_data)
+    #print(data)
+    #nodes = cs.individuals(data)
+    #print(nodes)
+    if label:
+        meta_data = cs.load_metadata("Datasets/metadata/metadata_"+file_name+".dat")
+    else:
+        meta_data = None
+        
+    if label:
+        graphs = cs.build_graphs(new_data,gap=gap,with_labels=label,meta_path="Datasets/metadata/metadata_"+file_name+".dat")
+    else:
+        graphs = cs.build_graphs(new_data,gap=gap,with_labels=label)                                # in "graphs" werden die static graphs gespeichert (das sind einfach temporal graphic snapshots at time t) (aus vielen temporal graphic snapshots kann man dann ETN bauen)
+        
+    #print(graphs)
+
+
+    # Count ETN or LETN and store the result
+    S = count_ETN(graphs,k,meta=meta_data)
+    S = {k: v for k, v in sorted(S.items(), key=lambda item: item[1], reverse=1)}               # das hier sortiert einfach die liste, sodass das etns was am meisten vorkommt ganz oben ist und das am wenigsten ganz unten
+
+    store_etns(S,file_name,gap,k,label=label)
+
+    #print(S)                                                                                    # diesen abschnitt mit dem Signature S muss ich mir nochmal genauer anschauen
+
+    # load etns 
+    SS = load_etns(file_name,gap,k,label=label)
+    assert(SS == S)
+
+
+    #S_array = list(S.keys())
+    #print(list(S.values()))
+    #print(from_ETNS_to_ETN(S_array[10],k=3,meta=None))                                         # hier verstehe ich noch nicht so ganz, warum der Graph so gezeichnet wird und why S_array[10] benutzt wird, allgemein nochmal anschauen
+    #draw_ETN(from_ETNS_to_ETN(S_array[0],k=2,meta=None),S_array[0][2:], ax ,multiple=False)                        
+
+    #print(list(S.keys()))
+    #print(list(S.values()))
+    #draw_barChart(list(S.keys()), list(S.values()))
+
+    '''
+    # plot 6 most frequent ETN
+    fig_per_row = 5
+    S_array = list(S.keys())
+    for i in range(0,5,fig_per_row):
+        plt.figure(figsize=(12,3))
+        for j in range(fig_per_row):
+            plt.subplot(1,fig_per_row,j+1)
+            ax = plt.subplot(1,fig_per_row,j+1)
+            print("count \t = \t",S[S_array[i+j]])
+            draw_ETN(from_ETNS_to_ETN(S_array[i+j],k,meta_data), S_array[i+j][2:], ax, multiple=True)
+
+        plt.show()
+
+    #draw_barChart(S_array[:][:5], list(S.values())[:5], k)
+    '''
+
+
+    # BUILD NULL MODELS                                                                         # verstehe die theorie hinter den null models noch nicht so richtig
+    def build_nm(graphs,n,file):
+        t = 0
+        to_save = []
+
+        f = open("null_models/"+file+"/"+file+"_"+str(n)+".txt", "w")                           # vorher war "a" statt "w", aber das war nerbig weil die files dann immer länger wurden weil einfach alles hinten rangepackt wurde, auch wenn man gap verändert hat
+
+
+        for g in graphs:
+            for e in g.edges():
+                s = str(t)+" "+str(e[0])+" "+str(e[1])+"\n"
+                f.write(s)
+            t = t + gap + 1                                                                     # kp why + 1, hab das mal mit 0.001 versucht aber dann kamen komplett crazy zahlen
+        f.close()
+
+    seed = 10
+    n = 5
+    null_models = shuffle_graphs(graphs,n,seed)
+    #print(len(null_models))
+
+    # store null models
+    c = 0
+
+    # open directory for the null models
+    directory = "null_models/" + file_name
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for graphs in null_models:
+        build_nm(graphs,c,file_name)
+        c = c + 1
 
 
 
-# Load the temporal graph as a sequence of static NetworkX graphs
-data = load_data("Datasets/ungerichtet/periodic_01/"+file_name)                   # hier nicht vergessen immer die Endungen (zB .txt) zu ändern, wenn oben file_name geändert wird
+    # Egocentric temporal motifs miner ETMM
 
-#print(data)
-
-def reorder_array(data):
-    
-    reordered_data = np.zeros_like(data)                            # Erstellt ein Array der gleichen Form wie data
-    
-    reordered_data[:, 0] = data[:, -1]                              # Verschiebt die letzte Spalte auf die erste
-    reordered_data[:, 1] = data[:, 0]                               # Verschiebt die erste Spalte auf die zweite
-    reordered_data[:, 2] = data[:, 1]                               # Verschiebt die zweite Spalte auf die dritte
-    
-    return reordered_data
+    # load precoputed etns
+    S = load_etns(file_name,gap,k,label)
 
 
-# Umordnen des Arrays
-reordered_data = reorder_array(data)
-#print(reordered_data)
+    # count etn in null models and store the results
+
+    counts = counts_ETN_null_models(null_models,S,k,label,meta_data,verbose=True)
+    store_etm_counts(counts,file_name,gap,k,label)
 
 
-def time_changed_data(reordered_data):                                              # da Charlottes Daten an der Stelle, wo eig die Zeit stehen muss, {'weight': 1} stehen haben und sie in der E-Mail meinte die Daten sind 
-    new_data = np.zeros_like(reordered_data)                                        # in der richtigen Reihenfolge platziert alle 0.5 Sekunden, habe ich dann diese Funktion programmiert 
-    new_data = new_data.astype(object)                                              # (Change to object type to allow mixed types)
-    new_data[:, 0] = np.arange(0, len(reordered_data[:,0]) * 0.5, 0.5)              # die an der Stelle von 0 hochzählt und in jeder inkrementation (0.5, 1, 1.5, ...) reinschreibt
-    new_data[:, 1] = reordered_data[:, 1]
-    new_data[:, 2] = reordered_data[:, 2]
-    return new_data
+    tmp = load_etm_count(file_name,gap,k,label)
+    assert(tmp == counts)
 
 
-new_data = time_changed_data(reordered_data)
-
-#print(new_data)
-#print(data)
-#nodes = cs.individuals(data)
-#print(nodes)
-if label:
-    meta_data = cs.load_metadata("Datasets/metadata/metadata_"+file_name+".dat")
-else:
-    meta_data = None
-    
-if label:
-    graphs = cs.build_graphs(new_data,gap=gap,with_labels=label,meta_path="Datasets/metadata/metadata_"+file_name+".dat")
-else:
-    graphs = cs.build_graphs(new_data,gap=gap,with_labels=label)                                # in "graphs" werden die static graphs gespeichert (das sind einfach temporal graphic snapshots at time t) (aus vielen temporal graphic snapshots kann man dann ETN bauen)
-    
-#print(graphs)
+    #print(counts)
 
 
-# Count ETN or LETN and store the result
-S = count_ETN(graphs,k,meta=meta_data)
-S = {k: v for k, v in sorted(S.items(), key=lambda item: item[1], reverse=1)}               # das hier sortiert einfach die liste, sodass das etns was am meisten vorkommt ganz oben ist und das am wenigsten ganz unten
+    '''
+    # APPLY STATISTICAL TEST                                                                    # den bre muss ich mir auch nochmal genauer anschauen
 
-store_etns(S,file_name,gap,k,label=label)
+    alpha=0.01
+    beta=0.1
+    gamma=5
 
-#print(S)                                                                                    # diesen abschnitt mit dem Signature S muss ich mir nochmal genauer anschauen
-
-# load etns 
-SS = load_etns(file_name,gap,k,label=label)
-assert(SS == S)
+    ETM = get_ETM(counts,alpha,beta,gamma)
 
 
-#S_array = list(S.keys())
-#print(list(S.values()))
-#print(from_ETNS_to_ETN(S_array[10],k=3,meta=None))                                         # hier verstehe ich noch nicht so ganz, warum der Graph so gezeichnet wird und why S_array[10] benutzt wird, allgemein nochmal anschauen
-#draw_ETN(from_ETNS_to_ETN(S_array[0],k=2,meta=None),S_array[0][2:], ax ,multiple=False)                        
+    # APPLY TEST BY ME
+    '''
+    N_G = np.array(list(counts.values()))[:,0]                                                  # N_G is the number of occurrences of sub-graph M in G
+    N_G0 = np.array(list(counts.values()))[:,1:]                                                # N_G0 is the average number of occurrences of sub-graph M in the random graphs (G0)
 
-#print(list(S.keys()))
-#print(list(S.values()))
-#draw_barChart(list(S.keys()), list(S.values()))
+    z = 0                                                                                       # iterator to go through the N_G0 list
+    for i in N_G:
+        if z == 2:
+            print("The number of occurences of some ETN in the original dataset is: " + str(i))
+            print("The number of occurences of the same ETN in the null models is: " + str(N_G0[z]))
+            avr_num = np.mean(N_G0[z])
+            print("The average number of occurences of the ETN in the null models is: " + str(avr_num))
+            diff = abs(i - avr_num)
+            print("The absolute deviation between them equals: " + str("{:.1f}".format(diff)))              # dieses "{:.1f}".format(diff) sorgt dafür, dass es nur 2 Nachkommastellen gibt
+            print("The percentage deviation equals: " + str((diff/i)*100))                                           
+            print( )
+        z = z + 1
 
-'''
-# plot 6 most frequent ETN
-fig_per_row = 5
-S_array = list(S.keys())
-for i in range(0,5,fig_per_row):
-    plt.figure(figsize=(12,3))
-    for j in range(fig_per_row):
-        plt.subplot(1,fig_per_row,j+1)
-        ax = plt.subplot(1,fig_per_row,j+1)
-        print("count \t = \t",S[S_array[i+j]])
-        draw_ETN(from_ETNS_to_ETN(S_array[i+j],k,meta_data), S_array[i+j][2:], ax, multiple=True)
+    #print(N_G)
+    #print(N_G0)
 
-    plt.show()
-
-#draw_barChart(S_array[:][:5], list(S.values())[:5], k)
-'''
-
-
-# BUILD NULL MODELS                                                                         # verstehe die theorie hinter den null models noch nicht so richtig
-def build_nm(graphs,n,file):
-    t = 0
-    to_save = []
-
-    f = open("null_models/"+file+"/"+file+"_"+str(n)+".txt", "w")                           # vorher war "a" statt "w", aber das war nerbig weil die files dann immer länger wurden weil einfach alles hinten rangepackt wurde, auch wenn man gap verändert hat
-
-
-    for g in graphs:
-        for e in g.edges():
-            s = str(t)+" "+str(e[0])+" "+str(e[1])+"\n"
-            f.write(s)
-        t = t + gap + 1                                                                     # kp why + 1, hab das mal mit 0.001 versucht aber dann kamen komplett crazy zahlen
-    f.close()
-
-seed = 10
-n = 5
-null_models = shuffle_graphs(graphs,n,seed)
-#print(len(null_models))
-
-# store null models
-c = 0
-
-# open directory for the null models
-directory = "null_models/" + file_name
-if not os.path.exists(directory):
-    os.makedirs(directory)
-
-for graphs in null_models:
-    build_nm(graphs,c,file_name)
-    c = c + 1
-
-
-
-# Egocentric temporal motifs miner ETMM
-
-# load precoputed etns
-S = load_etns(file_name,gap,k,label)
-
-
-# count etn in null models and store the results
-
-counts = counts_ETN_null_models(null_models,S,k,label,meta_data,verbose=True)
-store_etm_counts(counts,file_name,gap,k,label)
-
-
-tmp = load_etm_count(file_name,gap,k,label)
-assert(tmp == counts)
-
-
-#print(counts)
-
-
-'''
-# APPLY STATISTICAL TEST                                                                    # den bre muss ich mir auch nochmal genauer anschauen
-
-alpha=0.01
-beta=0.1
-gamma=5
-
-ETM = get_ETM(counts,alpha,beta,gamma)
-
-
-# APPLY TEST BY ME
-'''
-N_G = np.array(list(counts.values()))[:,0]                                                  # N_G is the number of occurrences of sub-graph M in G
-N_G0 = np.array(list(counts.values()))[:,1:]                                                # N_G0 is the average number of occurrences of sub-graph M in the random graphs (G0)
-
-z = 0                                                                                       # iterator to go through the N_G0 list
-for i in N_G:
-    if z == 1:
-        print("The number of occurences of some ETN in the original dataset is: " + str(i))
-        print("The number of occurences of the same ETN in the null models is: " + str(N_G0[z]))
-        avr_num = np.mean(N_G0[z])
-        print("The average number of occurences of the ETN in the null models is: " + str(avr_num))
-        diff = abs(i - avr_num)
-        print("The absolute deviation between them equals: " + str("{:.1f}".format(diff)))              # dieses "{:.1f}".format(diff) sorgt dafür, dass es nur 2 Nachkommastellen gibt
-        print("The percentage deviation equals: " + str((diff/i)*100))                                           
-        print( )
-    z = z + 1
-
-#print(N_G)
-#print(N_G0)
-
-'''
-fig_per_row = 5
-for i in range(0,fig_per_row,fig_per_row):
-    plt.figure(figsize=(12,3))
-    for j in range(fig_per_row):
-        plt.subplot(1,fig_per_row,j+1)
-        print("count \t = \t",ETM[i+j][1])
-        draw_ETN(from_ETNS_to_ETN(ETM[i+j][0],k,meta_data),multiple=True)
-    plt.show()
-'''
+    '''
+    fig_per_row = 5
+    for i in range(0,fig_per_row,fig_per_row):
+        plt.figure(figsize=(12,3))
+        for j in range(fig_per_row):
+            plt.subplot(1,fig_per_row,j+1)
+            print("count \t = \t",ETM[i+j][1])
+            draw_ETN(from_ETNS_to_ETN(ETM[i+j][0],k,meta_data),multiple=True)
+        plt.show()
+    '''
--- Xhemo_Zeitreihen_Graphen.py
+++ Xhemo_Zeitreihen_Graphen.py
@@ -3,26 +3,26 @@ import matplotlib.pyplot as plt
 
 # Zeitachse (t) und drei verschiedene Datensätze (x1, x2, x3)
 t = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-x1 = [817, 911, 846, 764, 817, 911, 846, 764, 817, 911, 846, 764, 817, 911, 846, 764, 817, 911, 846, 764, 817, 911, 846, 764, 817, 911, 846, 764, 817, 911, 846]
-x2 = [688, 777, 721, 643, 688, 777, 721, 643, 688, 777, 721, 643, 688, 777, 721, 643, 688, 777, 721, 643, 688, 777, 721, 643, 688, 777, 721, 643, 688, 777, 721]
-x3 = [571, 653, 606, 536, 571, 653, 606, 536, 571, 653, 606, 536, 571, 653, 606, 536, 571, 653, 606, 536, 571, 653, 606, 536, 571, 653, 606, 536, 571, 653, 606]
+x1 = [617.6, 651.9, 631.5, 494.1, 613.0, 574.6, 606.5, 654.4, 686.7, 599.8, 604.2, 656.8, 662.8, 661.4, 661.2, 688.9, 589.0, 572.6, 601.0, 571.8, 614.5, 600.9, 621.9, 594.2, 605.6, 633.9, 640.2, 547.8, 614.9, 629.5, 679.8]
+x2 = [606.9, 640.5, 628.6, 489.7, 611.6, 564.9, 595.9, 654.6, 683.7, 598.6, 596.5, 645.5, 659.5, 661.6, 650.1, 687.1, 586.2, 569.3, 590.6, 566.3, 611.2, 597.0, 618.1, 583.3, 601.3, 629.7, 634.3, 537.6, 602.9, 625.2, 661.6]
+x3 = [596.6, 626.9, 628.4, 483.4, 602.2, 562.3, 585.9, 642.9, 682.1, 596.1, 591.2, 633.1, 645.3, 652.2, 647.1, 686.7, 576.9, 556.6, 580.0, 560.8, 607.6, 587.2, 607.2, 573.6, 590.6, 626.2, 631.4, 534.7, 597.1, 610.2, 654.5]
 
 # Plot-Style (kein LaTeX, Schriftgrößen etc.)
 plt.rcParams['text.usetex'] = False
 plt.style.use('default')  # Zurücksetzen auf Standard-Design
 
 # Plot der drei Funktionen mit durchgezogenen Linien (nur Farben unterschiedlich)
-plt.plot(t, x1, 'k-', linewidth=2, label='ETNS 001-001-...')  # Schwarz, durchgezogen
-plt.plot(t, x2, 'r-', linewidth=2, label='ETNS 0001-0001-...')  # Rot, durchgezogen
-plt.plot(t, x3, 'b-', linewidth=2, label='ETNS 00001-00001-...')  # Blau, durchgezogen
+plt.plot(t, x1, 'k-', linewidth=2, label='ETNS 100-100 (k = 2)')  # Schwarz, durchgezogen
+plt.plot(t, x2, 'r-', linewidth=2, label='ETNS 1000-1000 (k = 3)')  # Rot, durchgezogen
+plt.plot(t, x3, 'b-', linewidth=2, label='ETNS 10000-10000 (k = 4)')  # Blau, durchgezogen
 
 # Achsenbeschriftungen & Titel
 plt.xlabel('t', fontsize=22)
 plt.ylabel('x', fontsize=22)
-plt.title('Evolution of the second ETNS in all three $k$', fontsize=22, pad=22)
+plt.title('Evolution of the third ETNS in all three $k$', fontsize=22, pad=22)
 
 # Y-Achse: Werte mit Prozentzeichen (%) formatieren
-#plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x}%'))
+plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x}%'))
 
 # Tick-Einstellungen (Größe, Richtung)
 plt.tick_params(axis='both', labelsize=22, width=1, length=6, direction='inout')
@@ -43,7 +43,7 @@ for spine in plt.gca().spines.values():
 
 # Achsenlimits
 plt.xlim(-1, 31)
-plt.ylim(0, 1400)
+plt.ylim(0, 900)
 
 # Layout optimieren und anzeigen
 plt.tight_layout()
